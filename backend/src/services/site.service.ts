1052fe0292845d6d6562f631f71c1376b0d62f0c
Ahmed A. Elsheikh <elsheikh41@gmail.com>
2026-07-28 09:18:16 +0300

    S1-T04 Update SiteService

--- backend/src/services/site.service.ts
+++ backend/src/services/site.service.ts
@@ -2,8 +2,8 @@ import { SiteRepository, Site } from "../repositories/site.repository";
 
 const repository = new SiteRepository();
 
-export function createSite(site: Site): void {
-    repository.create(site);
+export function createSite(site: Site): number {
+    return repository.create(site);
 }
 
 export function getSites(): Site[] {
