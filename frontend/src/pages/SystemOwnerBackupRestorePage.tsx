153ffd8433a73180fe786c2046966e735091b1be
Ahmed Elsheikh <53793040+elsheikh78@users.noreply.github.com>
2026-09-21 18:52:28 +0300

    style(backup-ui): format SystemOwnerBackupRestorePage.tsx

--- frontend/src/pages/SystemOwnerBackupRestorePage.tsx
+++ frontend/src/pages/SystemOwnerBackupRestorePage.tsx
@@ -10,7 +10,7 @@ export function SystemOwnerBackupRestorePage() {
         ...options,
         auth: "protected",
       }),
-    [apiClient]
+    [apiClient],
   );
   return (
     <BackupRestorePanel
